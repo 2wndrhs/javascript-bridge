3edb393f63dca5235861f83e03b3899273cc5964
2wndrhs <2wndrhs@gmail.com>
2022-12-08 15:00:44 +0900

    feat(bridge): 플레이어에게 다리 길이를 입력받는 기능 구현

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -1,9 +1,18 @@
+const InputView = require('../views/InputView');
 const OutputView = require('../views/OutputView');
 
 class GameController {
   start() {
     OutputView.printStarting();
+
+    this.#inputBridgeSize();
   }
+
+  #inputBridgeSize() {
+    InputView.readBridgeSize(this.onInputBridgeSize.bind(this));
+  }
+
+  onInputBridgeSize(size) {}
 }
 
 module.exports = GameController;
